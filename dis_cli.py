38f670d6a6f4274ac083834c7a58f69a2f90afd6
Josh Karpel <josh.karpel@gmail.com>
2020-09-10 12:11:05 -0500

    very basic functionality

--- dis_cli.py
+++ dis_cli.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 
+import dis
+import importlib
+import io
+import sys
+
 import click
 
 
-@click.command
-def cli() -> None:
-    pass
+@click.command()
+@click.argument("target")
+def cli(target) -> None:
+    module_path, object = target.rsplit(".", 1)
+
+    mod = importlib.import_module(module_path)
+    obj = getattr(mod, object)
+
+    out = io.StringIO()
+    dis.dis(obj, file=out)
+    out = out.getvalue()
+
+    print(out)
 
 
 if __name__ == "__main__":
-    exit(cli(prog_name="dis"))
+    sys.exit(cli(prog_name="dis"))
