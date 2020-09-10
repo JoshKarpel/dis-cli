f760bb4a60d60a7ff70798361f3228b5abfae76b
Josh Karpel <josh.karpel@gmail.com>
2020-09-10 15:52:28 -0500

    side-by-side display

--- dis_cli.py
+++ dis_cli.py
@@ -2,25 +2,49 @@
 
 import dis
 import importlib
-import io
+import inspect
+import itertools
 import sys
+from pathlib import Path
 
 import click
+from rich.console import Console
 
 
 @click.command()
 @click.argument("target")
 def cli(target) -> None:
+    sys.path.append(str(Path.cwd()))
+
+    console = Console(highlight=True, tab_size=4)
+
     module_path, object = target.rsplit(".", 1)
 
     mod = importlib.import_module(module_path)
     obj = getattr(mod, object)
 
-    out = io.StringIO()
-    dis.dis(obj, file=out)
-    out = out.getvalue()
+    bytecode = dis.Bytecode(obj)
+    source_lines, start_line = inspect.getsourcelines(obj)
 
-    print(out)
+    left, right = [], []
+    source_iter = (l.rstrip() for l in source_lines)
+
+    instructions = list(bytecode)
+    left.extend(itertools.islice(source_iter, instructions[0].starts_line - start_line))
+    right.extend([""] * len(left))
+
+    for instr in instructions:
+        if instr.starts_line is not None:
+            left.append(next(source_iter))
+        else:
+            left.append("")
+
+        right.append(instr._disassemble().rstrip())
+
+    longest_left = max(len(l) for l in left)
+
+    for l, r in zip(left, right):
+        console.print(f"{l:<{longest_left}} | {r}")
 
 
 if __name__ == "__main__":
