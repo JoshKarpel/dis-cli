d24c07ffc6e2cdaedb9c45acf43bb78598806fa2
Josh Karpel <josh.karpel@gmail.com>
2020-11-14 21:31:22 -0600

    more test cases; bump to v0.1.6 (#15)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,51 +10,50 @@ def test_smoke(cli):
     assert cli(["dis.dis"]).exit_code == 0
 
 
+ATTR_NAME = "wiz"
+CLASS_NAME = "Foo"
+CONST_NAME = "BANG"
 FUNC_NAME = "func"
-FUNC = f"""
+METHOD_NAME = "bar"
+NONE_NAME = "noon"
+
+SOURCE = f"""
+{CONST_NAME} = "string"
+{NONE_NAME} = None
+
 def {FUNC_NAME}():
     pass
-"""
 
-CLASS_NAME = "Foo"
-METHOD_NAME = "bar"
-CLASS = f"""
 class {CLASS_NAME}:
+    {ATTR_NAME} = True
+
     def {METHOD_NAME}(self):
         pass
 """
 
 
 @pytest.fixture
-def source_path_with_func(test_dir, filename) -> Path:
+def source_path(test_dir, filename) -> Path:
     source_path = test_dir / f"{filename}.py"
-    source_path.write_text(FUNC)
+    source_path.write_text(SOURCE)
 
     return source_path
 
 
-@pytest.fixture
-def source_path_with_class(test_dir, filename) -> Path:
-    source_path = test_dir / f"{filename}.py"
-    source_path.write_text(CLASS)
-
-    return source_path
-
-
-def test_runs_successfully_on_func_source(cli, source_path_with_func):
-    result = cli([f"{source_path_with_func.stem}.{FUNC_NAME}"])
+def test_runs_successfully_on_func_source(cli, source_path):
+    result = cli([f"{source_path.stem}.{FUNC_NAME}"])
 
     assert result.exit_code == 0
 
 
-def test_func_source_in_output(cli, source_path_with_func):
-    result = cli([f"{source_path_with_func.stem}.{FUNC_NAME}"])
+def test_func_source_in_output(cli, source_path):
+    result = cli([f"{source_path.stem}.{FUNC_NAME}"])
 
-    assert all([line in result.output for line in FUNC.splitlines()])
+    assert f"def {FUNC_NAME}():" in result.output
 
 
-def test_handle_missing_target_gracefully(cli, source_path_with_func):
-    result = cli([f"{source_path_with_func.stem}.{FUNC_NAME}osidjafoa"])
+def test_handle_missing_target_gracefully(cli, source_path):
+    result = cli([f"{source_path.stem}.{FUNC_NAME}osidjafoa"])
 
     assert result.exit_code == 1
     assert "osidjafoa" in result.output
@@ -66,7 +65,7 @@ def test_handle_missing_target_gracefully(cli, source_path_with_func):
 )
 def test_module_level_output_is_not_shown(cli, test_dir, filename, extra_source):
     source_path = test_dir / f"{filename}.py"
-    source_path.write_text(f"{extra_source}\n{FUNC}")
+    source_path.write_text(f"{extra_source}\n{SOURCE}")
     print(source_path.read_text())
 
     result = cli([f"{source_path.stem}.{FUNC_NAME}"])
@@ -78,7 +77,7 @@ def test_module_level_output_is_not_shown(cli, test_dir, filename, extra_source)
 @pytest.mark.parametrize("extra_source", ["raise Exception", "syntax error"])
 def test_module_level_error_is_handled_gracefully(cli, test_dir, filename, extra_source):
     source_path = test_dir / f"{filename}.py"
-    source_path.write_text(f"{extra_source}\n{FUNC}")
+    source_path.write_text(f"{extra_source}\n{SOURCE}")
     print(source_path.read_text())
 
     result = cli([f"{source_path.stem}.{FUNC_NAME}"])
@@ -87,7 +86,7 @@ def test_module_level_error_is_handled_gracefully(cli, test_dir, filename, extra
     assert "during import" in result.output
 
 
-def test_targetting_a_class_redirects_to_init(cli, test_dir, filename):
+def test_targeting_a_class_redirects_to_init(cli, test_dir, filename):
     source_path = test_dir / f"{filename}.py"
     source_path.write_text(
         textwrap.dedent(
@@ -106,6 +105,22 @@ def test_targetting_a_class_redirects_to_init(cli, test_dir, filename):
     assert "foobar" in result.output
 
 
+def test_can_target_method(cli, source_path):
+    result = cli([f"{source_path.stem}.{CLASS_NAME}.{METHOD_NAME}"])
+
+    assert result.exit_code == 0
+    assert METHOD_NAME in result.output
+
+
+def test_module_not_found(cli):
+    target = "fidsjofoiasjoifdj"
+    result = cli([target])
+
+    assert result.exit_code == 1
+    assert "No module named" in result.output
+    assert target in result.output
+
+
 @pytest.mark.parametrize(
     "target",
     [
@@ -117,22 +132,23 @@ def test_gracefully_cannot_disassemble_module(cli, target):
     result = cli([target])
 
     assert result.exit_code == 1
-    assert "Cannot disassemble modules" in result.output
+    assert "cannot be disassembled" in result.output
+    assert "module" in result.output
 
 
-def test_can_target_method(cli, source_path_with_class):
-    result = cli([f"{source_path_with_class.stem}.{CLASS_NAME}.{METHOD_NAME}"])
-
-    assert result.exit_code == 0
-
-
-def test_module_not_found(cli):
-    target = "fidsjofoiasjoifdj"
-    result = cli([target])
+@pytest.mark.parametrize(
+    "target",
+    [
+        f"{CONST_NAME}",
+        f"{CLASS_NAME}.{ATTR_NAME}",
+        f"{NONE_NAME}",
+    ],
+)
+def test_cannot_be_disassembled(cli, source_path, target):
+    result = cli([f"{source_path.stem}.{target}"])
 
     assert result.exit_code == 1
-    assert "No module named" in result.output
-    assert target in result.output
+    assert "cannot be disassembled" in result.output
 
 
 def test_version(cli):
